96ae7b0553652013eeb7c5a5057259561c20321e
ria-l <55597349+ria-l@users.noreply.github.com>
2022-01-25 20:18:00 -0800

    first commit

---
+++ precache-manifest.58262ae41d61f15a8c002ddaae008118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ad9a357073e872f9557dce20f2206b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e5918bb5be6959745d",
+    "url": "/static/css/2.0b5d44dd.chunk.css"
+  },
+  {
+    "revision": "fd4bb15e49dca094fb86",
+    "url": "/static/css/main.59c94fb7.chunk.css"
+  },
+  {
+    "revision": "b7e5918bb5be6959745d",
+    "url": "/static/js/2.9bd1caae.chunk.js"
+  },
+  {
+    "revision": "a2e233b5f4a7bf8c8dc6b87374c13f0f",
+    "url": "/static/js/2.9bd1caae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4bb15e49dca094fb86",
+    "url": "/static/js/main.791a5c02.chunk.js"
+  },
+  {
+    "revision": "3ea5d39dafc8ec51d31d",
+    "url": "/static/js/runtime-main.64c781b5.js"
+  }
+]);
